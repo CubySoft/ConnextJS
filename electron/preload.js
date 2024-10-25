2d483d7cbbca47045337fd6ec373288d8fc52f1d
owellandry <owellandry@gmail.com>
2024-10-25 03:01:29 -0500

    Update changes

--- electron/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  sendMessage: (data) => ipcRenderer.send('message', data),
-  onResponse: (callback) => ipcRenderer.on('response', callback),
-  shell: {
-    openExternal: (url) => ipcRenderer.send('open-external', url),
-  },
-});
